a5d91c003975ed7dbafae866963abdbac7a1d5ce
UmaShankar Sharma <umageorgian3541@gmail.com>
2026-02-20 16:30:24 +0530

    Converting to OOPs

--- edit.py
+++ edit.py
@@ -4,9 +4,66 @@ import re
 
 
 
-# class VideoEditor:
-#     DEFAULT_FONT = 'D:/DP/pyTon Env Testing/Chunk Five Print.otf'
-#     OUTPUT_SIZE = (1080, 1920)
+class VideoEditor:
+    DEFAULT_FONT = 'D:/DP/pyTon Env Testing/Chunk Five Print.otf'
+    OUTPUT_SIZE = (1080, 1920)
+
+    def __init__(self,videoName, font):
+        self.videoPath = videoName;
+        self.font = font;
+        self.vidName  = videoName[:-4]
+        self.subtitles = []
+        self.audio_src = None
+        self.video = VideoFileClip(videoName)
+
+    def _build_audio(self):
+        """Composite original + commentary audio if available."""
+
+        if self.audio_src:
+            commentary = AudioFileClip(self.audio_src)
+            print("Audio source:", self.audio_src)
+            return CompositeAudioClip([self.video.audio, commentary])
+        return self.video.audio
+    
+    def _build_subtitle(self):
+        """Build subtitle TextClips from loaded subtitle segments."""
+        text_clips = []
+        for seg in self.subtitle:
+            txt = (TextClip(text=seg['text'],font='D:/DP/pyTon Env Testing/Chunk Five Print.otf', font_size=70, color='white',stroke_color='black', stroke_width=2,method='caption', size=(1000, None))
+            .with_position(('center', 1400))
+            .with_start(seg['start'])
+            .with_duration(seg['end'] - seg['start']))
+        text_clips.append(txt)
+
+        return TextClip
+
+    
+
+  
+
+
+    def renderVideo(self):
+        """Compose and export the final video."""
+
+        # CROP the video
+        resized = self.video.resized(height=1920)
+        cropped = resized.cropped(x_center=resized.w/2, y_center=resized.h/2, width=1080, height=1920)
+
+
+          # Export
+        safe_filename = re.sub(r'[<>:"/\\|?*]', '', self.vidName[:-4])  # Remove invalid characters
+        filename = f"{safe_filename}_1.mp4"
+        final_video.write_videofile(filename, codec='libx264', audio_codec='aac', bitrate='583k')
+
+        
+
+
+
+
+
+        return output_path
+
+
 
 def editVideo(videoName, subtitle, audioSrc=None):
     # Load your video
@@ -49,7 +106,9 @@ def editVideo(videoName, subtitle, audioSrc=None):
     # Export
     safe_filename = re.sub(r'[<>:"/\\|?*]', '', videoName[:-4])  # Remove invalid characters
     filename = f"{safe_filename}_1.mp4"
-    final_video.write_videofile(filename, codec='libx264', audio_codec='aac', bitrate='2000k')
+    final_video.write_videofile(filename, codec='libx264', audio_codec='aac', bitrate='583k')
+
+def main():
 
 
 if __name__ == '__main__':
